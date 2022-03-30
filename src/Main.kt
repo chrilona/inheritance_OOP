8e70c0fbdd0f30fb0d898b659f717d3e14214f40
chrilona <christabelalona@gmail.com>
2022-03-30 08:24:46 +0300

    Inheritance of kotlin classes

---
+++ src/Main.kt
@@ -0,0 +1,41 @@
+fun main(){
+var car =Car("Toyota","corolla","blue",5)
+    var bus = Bus("Isuzu","latest","Yellow",52)
+    car.carry(7)
+    car.identity()
+    car.calculateParkingFees(2)
+    println(bus.maxTripFare(200.00))
+    println(bus.calculateParkingFees(2))
+
+}
+open class Car(var make:String,var model:String,var color:String,var capacity:Int){
+    fun carry(people:Int){
+        var x = people - capacity
+     if (people <= capacity){
+         println("carrying $capacity passengers")
+     }
+        else{
+            println("overcapacity by $x people")
+     }
+    }
+    fun identity(){
+       println("Iam a $color $make $model")
+    }
+   open fun calculateParkingFees(hours:Int): Int {
+        var parkingFees=hours*20
+        return parkingFees
+    }
+}
+class Bus(make: String,model: String,color: String,capacity: Int):Car(make,model,color,capacity){
+
+    fun maxTripFare(fare:Double): Double {
+       var maxFare = fare*capacity
+        return maxFare
+
+    }
+    override fun calculateParkingFees(hours: Int): Int {
+       // return super.calculateParkingFees(hours)
+        var fee = hours*capacity
+        return fee
+    }
+}
